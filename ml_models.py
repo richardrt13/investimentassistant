91883f1e6e36383188a14c5e13f5adcf89da90c1
Richard Tomé <richardrt13@gmail.com>
2024-08-03 16:15:10 -0300

    Update ml_models.py

--- ml_models.py
+++ ml_models.py
@@ -57,7 +57,7 @@ def train_model(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     
     # Manter as colunas antes de normalizar
-    columns = X_train.columns
+    columns = X_train.values
     
     # Normalizar os dados
     scaler = StandardScaler()
